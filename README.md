a05ba85d98c4464b78bcdd7c691a0f99bb367284
Alex Jimmy <alu0100728170@ull.edu.es>
2017-04-27 17:21:59 +0100

    Documentación añadida

---
+++ README.md
@@ -0,0 +1,16 @@
+# @ull-team-fernando-jimmy/ull-shape-triangle
+[![Build Status](https://travis-ci.org/ULL-ESIT-DSI-1617/creacion-de-paquetes-npm-jimmy-fernando-triangle.svg?branch=master)](https://travis-ci.org/ULL-ESIT-DSI-1617/creacion-de-paquetes-npm-jimmy-fernando-triangle)
+
+Calcula el área de un Triángulo.
+* [Documentación](docs/TRIANGLE.md)
+
+## Instalación
+
+```bash
+$ npm install --save @ull-team-fernando-jimmy/ull-shape
+```
+
+## Ejecución línea de comandos
+```bash
+$ node_modules/.bin/team-area Triangle '{width: 20, height: 10}'
+```
--- triangle.js
+++ triangle.js
@@ -1,6 +1,18 @@
 'use strict'
 let Shape = require('@ull-team-fernando-jimmy/ull-shape');
 
+/**
+* Clase de la forma Triangulo.
+* @extends Shape
+* @param {Object} options - Debe tener los siguiente atributos.
+* @param {Number} options.width - Establece el ancho del Triangulo.
+* @param {Number} options.height - Establece la altura del Triangulo.
+* @example
+*let Triangle = require('@ull-team-fernando-jimmy/ull-shape-triangle');
+*let triangle = new Triangle({width: 10, height: 20});
+*console.log(triangle.getArea()); //100
+*/
+
 class Triangle extends Shape {
     constructor(options) {
       super(options)
